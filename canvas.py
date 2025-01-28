24ebaef7a8478ec690e72e9f3978cc605c45019a
sportsfefi-07 <146748545+sportsfefi-07@users.noreply.github.com>
2025-01-27 18:31:32 -0600

    finished

--- canvas.py
+++ canvas.py
@@ -1,73 +1,43 @@
 import math
 import tkinter as tk
-
-
-
-
-
-# Draw a diagonal line from top-left to bottom-right
-
-m = 10
-x = 70
-b = 60
-y = m*x+b
-print (y)
-
-
+#find the distance between 2 points
+def listen_button():
+    if txtNum1.get()and txtNum2.get() and txtNum3.get() and txtNum4.get() :
+        x1 =350 + int(txtNum1.get())
+        y1 =350 + int(txtNum2.get())
+        x2 =350 + int(txtNum3.get())
+        y2 =350 + int(txtNum4.get())
+        distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        distance = round(distance,2)
+        lblAnswer.config(text=f"The distance between the two points is {distance}")
+    else:
+        lblAnswer.config(text=" Error you need to type 4 variables. ")
 window = tk.Tk() # create a window
 window.title("Slope intercept")
-window.geometry("1500x1500") # set the window size
-window.resizable(True, True) #
-canvas = tk.Canvas(window, width=1000, height=1000)
-
-
-
-def listen_button():
-    x1 =350 + int(txtNum1.get())
-    y1 =350 + int(txtNum2.get())
-    x2 =350 + int(txtNum3.get())
-    y2 =350 + int(txtNum4.get())
-    canvas.create_line(x1, y1, x2, y2)
-
-
+window.resizable(True, True)
+#Making GUI elements
 lblOne = tk.Label(window, text="x1: ")
 lblTwo = tk.Label(window, text="y1: ")
 lblThree = tk.Label(window, text="x2: ")
 lblFour = tk.Label(window, text="y2: ")
-lblFive = tk.Label(window, text="slope: ")
-lblSix = tk.Label(window, text="y intercept: ")
-
+lblAnswer =tk.Label(window, text="")
 txtNum1 = tk.Entry(window)
 txtNum2 = tk.Entry(window)
 txtNum3 = tk.Entry(window)
 txtNum4 = tk.Entry(window)
-txtNum5 = tk.Entry(window)
-txtNum6 = tk.Entry(window)
-
 btnCalc = tk.Button(window, text="Calculate",command=listen_button)
-#vertical line
-canvas.create_line(250,800,250,400)
-#horizintal line
-canvas.create_line(0,500,500,500)
-
-
-
 
 #####Building the Grid
 txtNum1.grid(row=0, column=0)
 txtNum2.grid(row=0, column=1)
 txtNum3.grid(row=0, column=2)
 txtNum4.grid(row=0, column=3)
-txtNum5.grid(row=0, column=4)
-txtNum6.grid(row=0, column=5)
 lblOne.grid(row=1, column=0)
 lblTwo.grid(row=1, column=1)
 lblThree.grid(row=1, column=2)
 lblFour.grid(row=1, column=3)
-lblFive.grid(row=1, column=4)
-lblSix.grid(row=1, column=5)
-btnCalc.grid(row=2, column=0)
-canvas.grid(row=3, column=4)
+btnCalc.grid(row=2, column=2)
+lblAnswer.grid(row=3,column=0,columnspan = 5)
 
 window.mainloop()
 
